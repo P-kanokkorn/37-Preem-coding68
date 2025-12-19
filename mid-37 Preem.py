7c30d6f77f3830448155dfd9b65ed67acc243cdc
P-kanokkorn <26629@bbw.ac.th>
2025-12-19 16:08:25 +0700

    สร้างโดยใช้ Colab

---
+++ mid-37 Preem.py
@@ -0,0 +1,69 @@
+{
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/P-kanokkorn/37-Preem-coding68/blob/main/mid-37%20Preem.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "Wf5KrEb6vrkR"
+      },
+      "source": [
+        "# ยินดีต้อนรับสู่ Colab"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "# สอบกลางภาค เทอม2 19/12/68\n",
+        "print(\"โปรแกรมที่ครูเฟิร์นสุดสวยคิดให้\")\n",
+        "number =int(input(\"ช่วยบอกตัวเลขที่คุณต้องการค้นหาด้วยค่ะ:\"))\n",
+        "\n",
+        "if number > 70:\n",
+        "   print(\"Positive class\")\n",
+        "elif number <= 70:\n",
+        "   print(\"Negative class\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "AWNKUpNYpxAX",
+        "outputId": "be1330ff-1899-4993-c542-f4043ad8e37b"
+      },
+      "execution_count": 27,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "โปรแกรมที่ครูเฟิร์นสุดสวยคิดให้\n",
+            "ช่วยบอกตัวเลขที่คุณต้องการค้นหาด้วยค่ะ:74\n",
+            "Positive class\n"
+          ]
+        }
+      ]
+    }
+  ],
+  "metadata": {
+    "colab": {
+      "name": "ยินดีต้อนรับสู่ Colab",
+      "toc_visible": true,
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
+}
